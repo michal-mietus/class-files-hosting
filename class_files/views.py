1429799404913f80aae8dad7f5bb4ff89cb985d2
goodude123 <assasinmcpl@gmail.com>
2019-02-24 13:27:41 +0100

    Section view displays all files which it contains.

--- class_files/views.py
+++ class_files/views.py
@@ -1,8 +1,19 @@
-from django.views.generic import ListView
-from .models import Section
+from django.views.generic import DetailView, ListView
+from .models import Section, File
 
 
-class Home(ListView):
-    template_name = 'class_files/home.html'
+class SectionView(DetailView):
     model = Section
+    context_object_name = 'section'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        section = Section.objects.get(pk=self.kwargs['pk'])
+        context['files'] = section.get_all_files()
+        return context
+
+
+class HomeView(ListView):
+    model = Section
+    template_name = 'class_files/home.html'
     context_object_name = 'sections'
