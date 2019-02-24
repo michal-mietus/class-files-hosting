3038d81a02b43264e917e2eb05eaa021fc7e3298
goodude123 <assasinmcpl@gmail.com>
2019-02-24 13:27:11 +0100

    Added section detail view link.

--- class_files/urls.py
+++ class_files/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'class_files'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('section/<int:pk>', views.SectionView.as_view(), name='section'),
 ]
