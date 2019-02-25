701fccf9f242351e6f7f574e825f571d469b11ab
goodude123 <assasinmcpl@gmail.com>
2019-02-25 18:07:12 +0100

    New methods: - create file name:   gets file object and creates filename which contains basename, date and extension. Get file extension method. Get formatted date:   - replace all spaces in date to underscores  Create dropbox file path.

--- class_files/views.py
+++ class_files/views.py
@@ -37,7 +37,27 @@ class UploadFile(FormView):
     template_name = 'class_files/upload_file.html'
     success_url = reverse_lazy('class_files:home')
 
-  
+
+        return dropbox.Dropbox(TOKEN)
+
+    def create_file_name(self, file):
+        """
+            Filename pattern: 
+                basename + '_' + date(spaces replaced with underscores) + file_extension
+        """
+        basename, extension = os.path.splitext(str(file))
+        filename = basename + '_' + self.get_formatted_date() + extension
+        return filename
+
+    def get_file_extension(self, file):
+        basename, extension = os.path.splitext(str(file))
+        return extension
+
+    def get_formatted_date(self):
+        return str(datetime.now()).replace(' ', '_')
+
+    def create_dropbox_file_path(self, file_name):
+        return self.get_dropbox_section_folder_path() + str(file_name)
 
     def get_dropbox_section_folder_path(self):
         return self.dropbox_folder_name + str(self.get_section()).lower() + '/' 
