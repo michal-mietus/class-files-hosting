ff00ae4f7d0968fe8eee30ba4f3cf23b9dbf032f
goodude123 <assasinmcpl@gmail.com>
2019-02-25 18:01:48 +0100

    HomeView for display all sections.

--- class_files/views.py
+++ class_files/views.py
@@ -1,5 +1,23 @@
-from django.views.generic import DetailView, ListView
+import os
+from datetime import datetime
+from django.views.generic import DetailView, ListView, FormView
+from django.urls import reverse_lazy
 from .models import Section, File
+from .forms import FileUploadForm
+from .api_token import TOKEN
+import dropbox
+
+
+class HomeView(ListView):
+    model = Section
+    template_name = 'class_files/home.html'
+    context_object_name = 'sections'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        for section in context['sections']:
+            section.icon_path = 'static/icons/' + section.icon_name
+        return context
 
 
 class SectionView(DetailView):
@@ -12,8 +30,3 @@ class SectionView(DetailView):
         context['files'] = section.get_all_files()
         return context
 
-
-class HomeView(ListView):
-    model = Section
-    template_name = 'class_files/home.html'
-    context_object_name = 'sections'
