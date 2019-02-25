a66a5290562bd6c0137da6a3ee328b68701ff23a
goodude123 <assasinmcpl@gmail.com>
2019-02-25 22:12:26 +0100

    Added section typeof view url.

--- class_files/urls.py
+++ class_files/urls.py
@@ -7,5 +7,6 @@ app_name = 'class_files'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('section/<int:pk>', views.SectionView.as_view(), name='section'),
+    path('section/<int:pk>/<str:type>', views.SectionFilesView.as_view(), name='section_files'),
     path('section/<int:pk>/upload', views.UploadFile.as_view(), name='upload_file')
 ]
