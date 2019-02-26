228c11c57a1a4182fc3b1187d89ebd9939008b7b
goodude123 <assasinmcpl@gmail.com>
2019-02-26 15:50:55 +0100

    Validator which checks is already created user with that username.

--- class_files/forms.py
+++ class_files/forms.py
@@ -5,6 +5,15 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.password_validation import validate_password
 
 
+def is_user_with_that_username(value):
+    try:
+        user = User.objects.get(username=value)
+    except Exception as e:
+        user = None
+    if user:
+        raise ValidationError(_('User with that login already exists!'))
+
+
 class FileUploadForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
