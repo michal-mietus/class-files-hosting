6eb585725c5a5f8afbbfda845c5e618e630b1998
goodude123 <assasinmcpl@gmail.com>
2019-02-26 15:50:13 +0100

    Login, Register form with placeholder and validators.

--- class_files/forms.py
+++ class_files/forms.py
@@ -1,4 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.password_validation import validate_password
+
 
 class FileUploadForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}), validators=[is_user_with_that_username])
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), validators=[validate_password])
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}), validators=[validate_password])
